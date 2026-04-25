22bf275d6539626a013f73004a1e1c4d4b85df39
Pobisk <PobiskWork@mail.ru>
2026-04-26 02:22:24 +0300

    Update main.py

--- backend/src/main.py
+++ backend/src/main.py
@@ -18,7 +18,7 @@ structlog.configure(
         structlog.processors.TimeStamper(fmt="iso"),
         structlog.processors.JSONRenderer(),
     ],
-    wrapper_class=structlog.make_filtering_bound_logger("info"),
+    wrapper_class=structlog.make_filtering_bound_logger(20),
 )
 
 logger = structlog.get_logger(__name__)
